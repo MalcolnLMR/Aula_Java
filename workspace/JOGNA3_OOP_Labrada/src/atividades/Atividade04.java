fcacc0714841e711ceb30ad6b28fe137853dd88f
MalcolnLMR <malcolnlucas@gmail.com>
2022-08-25 11:24:25 -0300

    Atividade04 e Projeto para Eclipse 2021

---
+++ workspace/JOGNA3_OOP_Labrada/src/atividades/Atividade04.java
@@ -0,0 +1,28 @@
+package atividades;
+
+import java.util.Scanner;
+
+public class Atividade04 {
+
+	public static void main(String[] args) {
+
+		System.out.println("Começando");
+		int number = -1;
+		int soma = 0;
+		int pos = 0, neg = 0;
+		Scanner scanner = new Scanner(System.in);
+		while(number != 0) {
+			number = scanner.nextInt();
+			soma += number;
+			if(number > 0) pos += 1;
+			if(number < 0) neg += 1;			
+		}
+		scanner.close();
+		int qtde = pos + neg;
+		double media = soma / qtde;
+		System.out.println("Soma: " + soma + " Positivos: " + pos + " Negativos: " + neg + " Média: " + media + "");
+	}
+	
+	
+
+}
