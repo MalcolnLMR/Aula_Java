c0be0be57dc40594c9fa4341591f70b12eedf092
MalcolnLMR <malcolnlucas@gmail.com>
2022-08-31 14:36:05 -0300

    Atividade04 fix

--- workspace/JOGNA3_OOP_Labrada/src/atividades/Atividade04.java
+++ workspace/JOGNA3_OOP_Labrada/src/atividades/Atividade04.java
@@ -5,7 +5,12 @@ import java.util.Scanner;
 public class Atividade04 {
 
 	public static void main(String[] args) {
-
+		getTenNumbers();
+		getNumbers();
+	}
+	
+	// Solicitar N número
+	public static void getNumbers() {
 		System.out.println("Começando");
 		int number = -1;
 		int soma = 0;
@@ -29,5 +34,43 @@ public class Atividade04 {
 							+ " Média: " 
 							+ media 
 							+ "");
+		
+	}
+	
+	// Solicitar 10 número
+	public static void getTenNumbers() {
+
+		System.out.println("Começando");
+		int count = 1, soma = 0; 
+		int maior = 0, menor = 0;
+		int number;
+		Scanner scanner = new Scanner(System.in);
+		
+		// Primeira Verificação (para definir o maior e menor)
+		number = scanner.nextInt();
+		maior = number;
+		menor = number;
+		soma += number;
+		
+		// Começa o loop
+		while(count < 10) {
+			number = scanner.nextInt();
+			soma += number;
+			if(number > maior) maior = number;
+			if(number < menor) menor = number;		
+			count += 1;
+		}
+		scanner.close();
+		double media = soma / count;
+		System.out.println("Soma: " 
+							+ soma 
+							+ " Média: " 
+							+ media 
+							+ " Maior: " 
+							+ maior 
+							+ " Menor: " 
+							+ menor 
+							+ "");
+		
 	}
 }
